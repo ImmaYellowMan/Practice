39d03da9e496c66659f07fbbd4195f07933c3bec
Ian Gomez <ian_gomez_50@yahoo.com.ph>
2019-05-02 14:55:36 -0500

    practice

---
+++ Unity_Practice-master/Unity_Practice-master/Assets/ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Text PushResultText;
+    public string text = "Youve Push The Button";
+
+    public void PushButton()
+    {
+        Debug.Log("youve push the button");
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
